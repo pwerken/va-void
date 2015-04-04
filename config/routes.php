5157c68c183279562671caa96e0626487cc91fd5
Peter van de Werken <pwerken@a-eskwadraat.nl>
2015-04-04 11:04:52 +0200

    follow upstream cakephp-app

--- config/routes.php
+++ config/routes.php
@@ -42,42 +42,50 @@ use Cake\Routing\Router;
 Router::defaultRouteClass('Route');
 
 Router::scope('/', function ($routes) {
+    /**
+     * Here, we are connecting '/' (base path) to a controller called 'Pages',
+     * its action called 'display', and we pass a param to select the view file
+     * to use (in this case, src/Template/Pages/home.ctp)...
+     */
+    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
 
-	$routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-	$routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
+    /**
+     * ...and connect the rest of 'Pages' controller's URLs.
+     */
+    $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
-	$routes->scope('/api', function ($routes) {
-		$routes->resources('Players');
-		$routes->resources('Characters');
-		$routes->resources('Items');
-		$routes->resources('Conditions');
-		$routes->resources('Powers');
-		$routes->resources('Skills');
+    $routes->scope('/api', function ($routes) {
+        $routes->resources('Players');
+        $routes->resources('Characters');
+        $routes->resources('Items');
+        $routes->resources('Conditions');
+        $routes->resources('Powers');
+        $routes->resources('Skills');
 
-		$routes->resources('Believes');
-		$routes->resources('Factions');
-		$routes->resources('Groups');
-		$routes->resources('Spells');
-		$routes->resources('Worlds');
-	});
+        $routes->resources('Believes');
+        $routes->resources('Factions');
+        $routes->resources('Groups');
+        $routes->resources('Spells');
+        $routes->resources('Worlds');
+    });
 
-/**
- * Connect catchall routes for all controllers.
- *
- * Using the argument `InflectedRoute`, the `fallbacks` method is a shortcut for
- *    `$routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'InflectedRoute']);`
- *    `$routes->connect('/:controller/:action/*', [], ['routeClass' => 'InflectedRoute']);`
- *
- * Any route class can be used with this method, such as:
- * - DashedRoute
- * - InflectedRoute
- * - Route
- * - Or your own route class
- *
- * You can remove these routes once you've connected the
- * routes you want in your application.
- */
-	$routes->fallbacks('InflectedRoute');
+    /**
+     * Connect catchall routes for all controllers.
+     *
+     * Using the argument `InflectedRoute`, the `fallbacks` method is a shortcut for
+     *    `$routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'InflectedRoute']);`
+     *    `$routes->connect('/:controller/:action/*', [], ['routeClass' => 'InflectedRoute']);`
+     *
+     * Any route class can be used with this method, such as:
+     * - DashedRoute
+     * - InflectedRoute
+     * - Route
+     * - Or your own route class
+     *
+     * You can remove these routes once you've connected the
+     * routes you want in your application.
+     */
+    $routes->fallbacks('InflectedRoute');
 });
 
 /**
--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -7,10 +7,10 @@
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         0.2.9
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link      http://cakephp.org CakePHP(tm) Project
+ * @since     0.2.9
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace App\Controller;
 
@@ -28,22 +28,23 @@ use Cake\ORM\ResultSet;
  *
  * @link http://book.cakephp.org/3.0/en/controllers.html#the-app-controller
  */
-class AppController extends Controller {
+class AppController extends Controller
+{
 
 	use \Crud\Controller\ControllerTrait;
 
 	public $helpers = [ 'Date' ];
 
-/**
- * Initialization hook method.
- *
- * Use this method to add common initialization code like loading components.
- *
- * @return void
- */
-	public function initialize() {
+	/**
+	 * Initialization hook method.
+	 *
+	 * Use this method to add common initialization code like loading components.
+	 *
+	 * @return void
+	 */
+	public function initialize()
+	{
 		parent::initialize();
-
 		$this->loadComponent('Flash');
 		$this->loadComponent('RequestHandler');
 		$this->loadComponent('Crud.Crud',
@@ -60,10 +61,14 @@ class AppController extends Controller {
 			]
 		);
 
-		array_walk($this->request->data, array($this, 'removeEmptyDate'));
+		if($this->request->is('json'))
+			$this->request->data = $this->request->input('json_decode', true);
+		if(!is_null($this->request->data))
+			array_walk($this->request->data, array($this, 'removeEmptyDate'));
 	}
 
-	private function removeEmptyDate(&$value, $key) {
+	private function removeEmptyDate(&$value, $key)
+	{
 		if(!is_array($value)) return;
 		if(count($value) == 3 &&
 			isset($value['year'], $value['month'], $value['day']) &&
@@ -74,7 +79,8 @@ class AppController extends Controller {
 		}
 	}
 
-	public function beforeRender(Event $event) {
+	public function beforeRender(Event $event)
+	{
 		parent::beforeRender($event);
 
 		if($this->request->is('json')) {
