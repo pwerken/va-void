d51d61b289f9b2279c6d788a0e4a388abf6d902f
Peter van de Werken <pwerken@a-eskwadraat.nl>
2015-04-19 15:17:52 +0200

    rewrite route generation

--- config/routes.php
+++ config/routes.php
@@ -20,6 +20,7 @@
 
 use Cake\Core\Plugin;
 use Cake\Routing\Router;
+use Cake\Utility\Inflector;
 
 /**
  * The default class to use for all routes
@@ -55,30 +56,99 @@ Router::scope('/', function ($routes) {
     $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
     $routes->scope('/api', function ($routes) {
-        $routes->resources('Players');
-        $routes->resources('Characters',
-                [ 'connectOptions' =>
-                    [ 'plin' => '[0-9]+'
-                    , 'chin' => '[0-9]+'
-                    , 'pass' => ['plin', 'chin']
-                    ]
-                , 'map' =>
-                    [ 'view' =>
-                        [ 'action' => 'view'
-                        , 'method' => 'GET'
-                        , 'path' => ':plin/:chin'
-                ]   ]   ]);
 
-        $routes->resources('Items');
-        $routes->resources('Conditions');
-        $routes->resources('Powers');
-        $routes->resources('Skills');
+function getKeys($controller) {
+	switch($controller) {
+	case 'Players':     return [ 'plin' ];
+	case 'Characters':  return [ 'plin', 'chin' ];
+	case 'Items':       return [ 'itin' ];
+	case 'Conditions':  return [ 'coin' ];
+	case 'Powers':      return [ 'poin' ];
+	default:            return [ 'id' ];
+	}
+}
+function rest($routes, $name, $subs = [], $rels = []) {
+	$lcName = Inflector::underscore($name);
 
-        $routes->resources('Believes');
-        $routes->resources('Factions');
-        $routes->resources('Groups');
-        $routes->resources('Spells');
-        $routes->resources('Worlds');
+	$routeOptions = [];
+	foreach(getKeys($name) as $key) {
+		$routeOptions[$key] = '[0-9]+';
+		$routeOptions['pass'][] = $key;
+	}
+	$path = ':'.implode('/:', $routeOptions['pass']);
+
+	$map =  [ 'index'   => [ '_method' => [ 'GET' ]         , 'path' => '' ]
+			, 'add'     => [ '_method' => [ 'PUT' ]         , 'path' => '' ]
+			, 'view'    => [ '_method' => [ 'GET' ]         , 'path' => $path ]
+			, 'edit'    => [ '_method' => [ 'PUT', 'PATCH' ], 'path' => $path ]
+			, 'delete'  => [ '_method' => [ 'DELETE' ]      , 'path' => $path ]
+			];
+
+	foreach($map as $method => $params) {
+		$params['controller'] = $name;
+		$params['action'] = $method;
+
+		$url = '/'.$lcName.'/'.$params['path'];
+
+		$routes->connect($url, $params, $routeOptions);
+	}
+
+	foreach($subs as $sub) {
+		$sub = Inflector::underscore($sub);
+
+		$params = [];
+		$params['_method'] = 'GET';
+		$params['controller'] = $name;
+		$params['action'] = $sub.'Index';
+
+		$routes->connect($url . '/' . $sub, $params, $routeOptions);
+	}
+
+	foreach($rels as $rel) {
+		$lcRel = Inflector::underscore($rel);
+
+		$routeOptions2 = $routeOptions;
+		$path = [];
+		foreach(getKeys($rel) as $key) {
+			$routeOptions2[$key] = '[0-9]+';
+			$routeOptions2['pass'][] = $key;
+			$path[] = $key;
+		}
+		$path = ':'.implode('/:', $path);
+
+		$map =  [ 'index'   => [ '_method' => [ 'GET' ]         , 'path' => '' ]
+				, 'add'     => [ '_method' => [ 'PUT' ]         , 'path' => '' ]
+				, 'view'    => [ '_method' => [ 'GET' ]         , 'path' => $path ]
+				, 'edit'    => [ '_method' => [ 'PUT', 'PATCH' ], 'path' => $path ]
+				, 'delete'  => [ '_method' => [ 'DELETE' ]      , 'path' => $path ]
+				];
+
+		foreach($map as $method => $params) {
+			$params['controller'] = $name;
+			$params['action'] = $lcRel.ucFirst($method);
+
+			$urlRel = $url . '/' . $lcRel . '/' . $params['path'];
+
+			$routes->connect($urlRel, $params, $routeOptions2);
+		}
+	}
+}
+
+        rest($routes, 'Players');
+        rest($routes, 'Characters'
+                        , [ 'Items']
+                        , [ 'Conditions', 'Powers', 'Skills' ]
+                        );
+        rest($routes, 'Items');
+        rest($routes, 'Conditions');
+        rest($routes, 'Powers');
+        rest($routes, 'Skills');
+
+        rest($routes, 'Believes');
+        rest($routes, 'Factions');
+        rest($routes, 'Groups');
+        rest($routes, 'Spells');
+        rest($routes, 'Worlds');
     });
 
     /**
