762310f19177ad957dbcdbc07c493fb82d3c0b66
Peter van de Werken <pwerken@a-eskwadraat.nl>
2016-03-11 08:59:25 +0100

    codingstyle of ApiView, remove unused Log

--- src/View/ApiView.php
+++ src/View/ApiView.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\View;
 
 use Cake\Core\Configure;
@@ -8,8 +7,10 @@ use Cake\ORM\ResultSet;
 use Cake\Utility\Inflector;
 use Cake\View\View;
 
-class ApiView extends View
+class ApiView
+	extends View
 {
+
 	private $_aliases =
 	[ 'Character'           => ['player_id' => 'plin']
 	, 'Condition'           => ['id' => 'coin']
@@ -19,7 +20,7 @@ class ApiView extends View
 	];
 
 	private $_compact =
-    [ 'AttributesItem'      => ['attribute', 'item']
+	[ 'AttributesItem'      => ['attribute', 'item']
 	, 'Character'           => ['player_id', 'chin', 'name']
     , 'CharactersCondition' => ['expiry', 'character', 'condition']
     , 'CharactersPower'     => ['expiry', 'character', 'power']
@@ -47,64 +48,61 @@ class ApiView extends View
 		}
 
 		$jsonOptions = JSON_HEX_TAG|JSON_HEX_APOS|JSON_HEX_AMP|JSON_HEX_QUOT;
-		if (Configure::read('debug')) {
+		if(Configure::read('debug'))
 			$jsonOptions = $jsonOptions | JSON_PRETTY_PRINT;
-		}
+
 		return json_encode($data, $jsonOptions);
 	}
 
-	private function _class($value) {
+	private function _class($value)
+	{
 		return join('', array_slice(explode('\\', get_class($value)), -1));
 	}
 
-	private function _jsonUrl($value = null) {
-        if(is_null($value)) {
+	private function _jsonUrl($obj = null)
+	{
+		if(is_null($obj))
 			return '/'.$this->request->url;
-		}
 
-        $class = $this->_class($value);
-        if($class == 'Character')
-            return '/characters/'.$value->player_id.'/'.$value->chin;
+		$class = $this->_class($obj);
+		if($class == 'Character')
+			return '/characters/'.$obj->player_id.'/'.$obj->chin;
 		if($class == 'AttributesItem')
-			return $this->_jsonUrlJoin($value
-							, $value->item, $value->attribute);
+			return $this->_jsonUrlJoin($obj, $obj->item, $obj->attribute);
 		if($class == 'CharactersCondition')
-			return $this->_jsonUrlJoin($value
-							, $value->character, $value->condition);
+			return $this->_jsonUrlJoin($obj, $obj->character, $obj->condition);
 		if($class == 'CharactersPower')
-			return $this->_jsonUrlJoin($value
-							, $value->character, $value->power);
+			return $this->_jsonUrlJoin($obj, $obj->character, $obj->power);
 		if($class == 'CharactersSkill')
-			return $this->_jsonUrlJoin($value
-							, $value->character, $value->skill);
+			return $this->_jsonUrlJoin($obj, $obj->character, $obj->skill);
 		if($class == 'CharactersSpell')
-			return $this->_jsonUrlJoin($value
-							, $value->character, $value->spell);
+			return $this->_jsonUrlJoin($obj, $obj->character, $obj->spell);
 
-        return '/'.strtolower(Inflector::pluralize($class)).'/'.$value->get('id');
+		return '/'.strtolower(Inflector::pluralize($class)).'/'.$obj->id;
 	}
-	private function _jsonUrlJoin($join, $a, $b) {
-        $class = $this->_class($join);
+	private function _jsonUrlJoin($join, $a, $b)
+	{
+		$class = $this->_class($join);
 		$aCls  = $this->_class($a);	$aUrl = $this->_jsonUrl($a);
 		$bCls  = $this->_class($b); $bUrl = $this->_jsonUrl($b);
 		switch($class) {
-        case 'AttributesItem':
+		case 'AttributesItem':
 			if($aCls == 'Item' && $bCls == 'Attribute') return $aUrl.$bUrl;
 			if($bCls == 'Item' && $aCls == 'Attribute') return $bUrl.$aUrl;
 			break;
-        case 'CharactersCondition':
+		case 'CharactersCondition':
 			if($aCls == 'Character' && $bCls == 'Condition') return $aUrl.$bUrl;
 			if($bCls == 'Character' && $aCls == 'Condition') return $bUrl.$aUrl;
 			break;
-        case 'CharactersPower':
+		case 'CharactersPower':
 			if($aCls == 'Character' && $bCls == 'Power') return $aUrl.$bUrl;
 			if($bCls == 'Character' && $aCls == 'Power') return $bUrl.$aUrl;
 			break;
-        case 'CharactersSkill':
+		case 'CharactersSkill':
 			if($aCls == 'Character' && $bCls == 'Skill') return $aUrl.$bUrl;
 			if($bCls == 'Character' && $aCls == 'Skill') return $bUrl.$aUrl;
 			break;
-        case 'CharactersSpell':
+		case 'CharactersSpell':
 			if($aCls == 'Character' && $bCls == 'Spell') return $aUrl.$bUrl;
 			if($bCls == 'Character' && $aCls == 'Spell') return $bUrl.$aUrl;
 			break;
@@ -132,9 +130,9 @@ class ApiView extends View
 				$value = $this->_jsonCompact($value);
 			}
 
-			$camel = Inflector::camelize($key);
-			if(method_exists($obj, 'label'.$camel))
-				$value = call_user_func([$obj, 'label'.$camel], $value);
+			$label = Inflector::camelize("label_".$key);
+			if(method_exists($obj, $label))
+				$value = call_user_func([$obj, $label], $value);
 
 			if(isset($this->_aliases[$class][$key]))
 				$key = $this->_aliases[$class][$key];
@@ -142,10 +140,11 @@ class ApiView extends View
 		}
 		return $result;
 	}
-	private function _jsonList($objs, $parent = null) {
+	private function _jsonList($objs, $parent = null)
+	{
 		$result = [];
 		$result['class'] = 'List';
-        $result['url'] = $this->_jsonUrl();
+		$result['url'] = $this->_jsonUrl();
 
 		$remove = '';
 		if($parent) {
@@ -175,29 +174,30 @@ class ApiView extends View
 		}
 		return $result;
 	}
-	private function _jsonCompact($obj) {
+	private function _jsonCompact($obj)
+	{
 		if(!($obj instanceof EntityInterface))
 			return $obj;
 
-        $class = $this->_class($obj);
+		$class = $this->_class($obj);
 
 		$properties = ['id', 'name'];
 		if(isset($this->_compact[$class]))
 			$properties = $this->_compact[$class];
 
-        if(empty($properties))
+		if(empty($properties))
 			return $obj->get('name');
 
-        $result = [];
-        $result['class'] = $class;
-        $result['url'] = $this->_jsonUrl($obj);
-        foreach($properties as $key) {
-            $value = $this->_jsonCompact($obj->get($key));
+		$result = [];
+		$result['class'] = $class;
+		$result['url'] = $this->_jsonUrl($obj);
+		foreach($properties as $key) {
+			$value = $this->_jsonCompact($obj->get($key));
 			if(isset($this->_aliases[$class][$key]))
 				$key = $this->_aliases[$class][$key];
-            $result[$key] = $value;
-        }
-
-		return $result; #$this->_jsonData($value); #$value;
+			$result[$key] = $value;
+		}
+		return $result;
 	}
+
 }
