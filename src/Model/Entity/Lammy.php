dc5cc4235c2d707298e317a7deac5dabf13c706b
Peter van de Werken <pwerken@a-eskwadraat.nl>
2016-03-21 21:02:49 +0100

    Lammy->getTarget() get the referenced entity

--- src/Model/Entity/Lammy.php
+++ src/Model/Entity/Lammy.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Model\Entity;
 
+use Cake\ORM\TableRegistry;
+use Cake\Utility\Inflector;
+
 class Lammy
 	extends AppEntity
 {
@@ -8,5 +11,21 @@ class Lammy
 	protected $_defaults =
 			[ 'printed'     => false
 			];
+
+	public function getTarget()
+	{
+		$name = Inflector::pluralize($this->entity);
+		$table = TableRegistry::get($name);
+
+		$keys = [$this->key1, $this->key2];
+		$primary = $table->primaryKey();
+		if(!is_array($primary)) $primary = [$primary];
+
+		$where = [];
+		foreach($primary as $i => $id)
+			$where[$name.'.'.$id] = $keys[$i];
+
+		return $table->find('WithContain')->where($where)->first();
+	}
 
 }
