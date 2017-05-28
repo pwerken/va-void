702c5eff1af8bb765263a0420a1258978118fcf7
Peter van de Werken <pwerken@a-eskwadraat.nl>
2017-05-28 13:48:52 +0200

    cors doesn't need an extra check

--- src/Routing/Middleware/CorsMiddleware.php
+++ src/Routing/Middleware/CorsMiddleware.php
@@ -10,21 +10,18 @@ class CorsMiddleware
 
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $next)
 	{
-        if($request->getHeader('Origin')) {
-			$response->cors($request)
-				->allowOrigin(['*.the-vortex.nl', 'yvo.muze.nl'])
-				->allowCredentials(true)
-				->allowMethods(['GET','PUT','DELETE','POST','OPTIONS'])
-				->allowHeaders(['x-requested-with', 'Content-Type', 'origin'
-						, 'authorization', 'accept', 'client-security-token'])
-				->exposeHeaders(['Location'])
-				->build();
+		$response->cors($request)
+			->allowOrigin(['*.the-vortex.nl', 'yvo.muze.nl'])
+			->allowCredentials(true)
+			->allowMethods(['GET','PUT','DELETE','POST','OPTIONS'])
+			->allowHeaders(['x-requested-with', 'Content-Type', 'origin'
+					, 'authorization', 'accept', 'client-security-token'])
+			->exposeHeaders(['Location'])
+			->build();
 
-            if($request->is('options')) {
-                return $response;
-            }
-        }
-
+		if($request->is('options')) {
+			return $response;
+		}
         return $next($request, $response);
     }
 
