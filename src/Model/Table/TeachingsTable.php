06c8a7783f4df6a18c123d7e4199318e2b900863
Peter van de Werken <pwerken@a-eskwadraat.nl>
2017-06-09 22:02:37 +0200

    Teaching: add validation rules

--- src/Model/Table/TeachingsTable.php
+++ src/Model/Table/TeachingsTable.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Model\Table;
 
+use Cake\ORM\RulesChecker;
+use Cake\Validation\Validator;
+
 class TeachingsTable
 	extends AppTable
 {
@@ -26,6 +29,43 @@ class TeachingsTable
 	public function orderBy()
 	{
 		return	[ 'student_id' => 'ASC' ];
+	}
+
+	public function validationDefault(Validator $validator)
+	{
+		$validator->allowEmpty('id', 'create');
+		$validator->notEmpty('student_id');
+		$validator->notEmpty('teacher_id');
+		$validator->notEmpty('skill_id');
+		$validator->notEmpty('xp');
+		$validator->notEmpty('started_id');
+		$validator->notEmpty('updated_id');
+
+		$validator->add('student_id', 'valid', ['rule' => 'numeric']);
+		$validator->add('teacher_id', 'valid', ['rule' => 'numeric']);
+		$validator->add('xp', 'valid', ['rule' => ['custom', '/^[0-9]*([\.][05])?$/']]);
+		$validator->add('skill_id', 'valid', ['rule' => 'numeric']);
+		$validator->add('started_id', 'valid', ['rule' => 'numeric']);
+		$validator->add('updated_id', 'valid', ['rule' => 'numeric']);
+
+		$validator->requirePresence('student_id', 'create');
+		$validator->requirePresence('teacher_id', 'create');
+		$validator->requirePresence('skill_id', 'create');
+		$validator->requirePresence('started_id', 'create');
+		$validator->requirePresence('updated_id', 'create');
+
+		return $validator;
+	}
+
+	public function buildRules(RulesChecker $rules)
+	{
+		$rules->add($rules->existsIn('student_id', 'Student'));
+		$rules->add($rules->existsIn('teacher_id', 'Teacher'));
+		$rules->add($rules->existsIn('skill_id',   'Skills'));
+		$rules->add($rules->existsIn('started_id', 'Started'));
+		$rules->add($rules->existsIn('updated_id', 'Updated'));
+
+		return $rules;
 	}
 
 	public function findStudent($plin, $chin)
