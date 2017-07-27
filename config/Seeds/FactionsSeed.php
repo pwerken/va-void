4b6be9c0e40bad33f6ee2c3811e2f50129d69fbb
Peter van de Werken <pwerken@a-eskwadraat.nl>
2017-07-27 19:05:58 +0200

    add some more db seed files

---
+++ config/Seeds/FactionsSeed.php
@@ -0,0 +1,72 @@
+<?php
+use Migrations\AbstractSeed;
+
+/**
+ * Factions seed.
+ */
+class FactionsSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'id' => '1',
+                'name' => '-',
+                'modified' => NULL,
+            ],
+            [
+                'id' => '2',
+                'name' => 'Gargoyle',
+                'modified' => NULL,
+            ],
+            [
+                'id' => '3',
+                'name' => 'Wyvern',
+                'modified' => NULL,
+            ],
+            [
+                'id' => '4',
+                'name' => 'Phoenix',
+                'modified' => NULL,
+            ],
+            [
+                'id' => '5',
+                'name' => 'Leviathan',
+                'modified' => NULL,
+            ],
+            [
+                'id' => '6',
+                'name' => 'Ent',
+                'modified' => NULL,
+            ],
+            [
+                'id' => '7',
+                'name' => 'Unicorn',
+                'modified' => NULL,
+            ],
+            [
+                'id' => '8',
+                'name' => 'Naga',
+                'modified' => NULL,
+            ],
+            [
+                'id' => '9',
+                'name' => 'Metal',
+                'modified' => NULL,
+            ],
+        ];
+
+        $table = $this->table('factions');
+        $table->insert($data)->save();
+    }
+}
---
+++ config/Seeds/SpellsSeed.php
@@ -0,0 +1,111 @@
+<?php
+use Migrations\AbstractSeed;
+
+/**
+ * Spells seed.
+ */
+class SpellsSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'id' => '1',
+                'name' => 'Air',
+                'short' => 'Air',
+                'spiritual' => '0',
+            ],
+            [
+                'id' => '2',
+                'name' => 'Earth',
+                'short' => 'Earth',
+                'spiritual' => '0',
+            ],
+            [
+                'id' => '3',
+                'name' => 'Fire',
+                'short' => 'Fire',
+                'spiritual' => '0',
+            ],
+            [
+                'id' => '4',
+                'name' => 'Metal',
+                'short' => 'Metal',
+                'spiritual' => '0',
+            ],
+            [
+                'id' => '5',
+                'name' => 'Water',
+                'short' => 'Water',
+                'spiritual' => '0',
+            ],
+            [
+                'id' => '6',
+                'name' => 'Wood',
+                'short' => 'Wood',
+                'spiritual' => '0',
+            ],
+            [
+                'id' => '7',
+                'name' => 'Body/Life',
+                'short' => 'B/L',
+                'spiritual' => '1',
+            ],
+            [
+                'id' => '8',
+                'name' => 'Chaos/Destruction',
+                'short' => 'C/D',
+                'spiritual' => '1',
+            ],
+            [
+                'id' => '9',
+                'name' => 'Civilization/Order',
+                'short' => 'C/O',
+                'spiritual' => '1',
+            ],
+            [
+                'id' => '10',
+                'name' => 'Death/Soul',
+                'short' => 'D/S',
+                'spiritual' => '1',
+            ],
+            [
+                'id' => '11',
+                'name' => 'Evil/Darkness',
+                'short' => 'E/D',
+                'spiritual' => '1',
+            ],
+            [
+                'id' => '12',
+                'name' => 'Good/Light',
+                'short' => 'G/L',
+                'spiritual' => '1',
+            ],
+            [
+                'id' => '13',
+                'name' => 'Mind/Knowledge',
+                'short' => 'M/K',
+                'spiritual' => '1',
+            ],
+            [
+                'id' => '14',
+                'name' => 'Nature/Creation',
+                'short' => 'N/C',
+                'spiritual' => '1',
+            ],
+        ];
+
+        $table = $this->table('spells');
+        $table->insert($data)->save();
+    }
+}
