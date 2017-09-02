2edfaff27eac5c1311805f419dde89780a3464f0
Peter van de Werken <pwerken@a-eskwadraat.nl>
2017-09-02 21:11:30 +0200

    Admin/password: don't echo password as it's typed

--- src/Shell/AdminShell.php
+++ src/Shell/AdminShell.php
@@ -100,7 +100,7 @@ class AdminShell extends Shell
 			$player->password = NULL;
 			$msg = 'Password removed';
 		} else {
-			$player->set('password', $this->in('Password?'));
+			$player->set('password', $this->prompt_silent());
 			$msg = 'Password set';
 		}
 
@@ -160,5 +160,31 @@ class AdminShell extends Shell
 		}
 
 		return $parser;
+	}
+
+
+	/**
+	 * Interactively prompts for input without echoing to the terminal.
+	 * Requires a bash shell or Windows and won't work with
+	 * safe_mode settings (Uses `shell_exec`)
+	 */
+	private function prompt_silent($prompt = "Enter Password:")
+	{
+		if (preg_match('/^win/i', PHP_OS)) {
+			$vbscript = sys_get_temp_dir() . 'prompt_password.vbs';
+			file_put_contents(
+				$vbscript, 'wscript.echo(InputBox("'
+				. addslashes($prompt)
+				. '", "", "password here"))');
+			$command = "cscript //nologo " . escapeshellarg($vbscript);
+			$password = rtrim(shell_exec($command));
+			unlink($vbscript);
+			return $password;
+		} else {
+			$this->out('<question>'.$prompt.'</question>');
+			$command = "/usr/bin/env bash -c 'read -s -p \"\" mypassword && echo \$mypassword'";
+			$password = rtrim(shell_exec($command));
+			return $password;
+		}
 	}
 }
