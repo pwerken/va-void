e4af80e359db421f67c10b5e3c2a0abd640be7dc
Peter van de Werken <peter@van-de-werken.nl>
2023-06-10 23:26:36 +0200

    migrations: fix audit table rename

--- config/Migrations/20170905204845_RenameAuditsToHistory.php
+++ config/Migrations/20170905204845_RenameAuditsToHistory.php
@@ -6,11 +6,11 @@ class RenameAuditsToHistory extends AbstractMigration
 
 	public function up()
 	{
-		$this->table('audits')->rename('history');
+		$this->table('audits')->rename('history')->update();
 	}
 
 	public function down()
 	{
-		$this->table('history')->rename('audits');
+		$this->table('history')->rename('audits')->update();
 	}
 }
