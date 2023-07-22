6619ed90281652027fc2cbfded9a1e9bcc17834a
Peter van de Werken <peter@van-de-werken.nl>
2023-07-22 14:54:40 +0200

    tests: remove failed merge lines

--- tests/Fixture/PlayersFixture.php
+++ tests/Fixture/PlayersFixture.php
@@ -9,32 +9,6 @@ use Cake\TestSuite\Fixture\TestFixture;
 class PlayersFixture
 	extends TestFixture
 {
-<<<<<<< HEAD
-	public $fields =
-		[ 'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null]
-		, 'role' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null]
-		, 'password' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null]
-		, 'first_name' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null]
-		, 'insertion' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null]
-		, 'last_name' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null]
-		, 'gender' => ['type' => 'string', 'length' => 1, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null]
-		, 'date_of_birth' => ['type' => 'date', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null]
-		, 'modified' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null]
-		, 'modifier_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null]
-		, '_indexes' =>
-			[
-			]
-		, '_constraints' =>
-			[ 'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []]
-			]
-		, '_options' =>
-			[ 'engine' => 'InnoDB'
-			, 'collation' => 'utf8_general_ci'
-			]
-		];
-
-=======
->>>>>>> 0533d1f (tests: use migration for testdb setup instead of fixtures)
 	public function init(): void
 	{
 		$hasher = new DefaultPasswordHasher();
--- tests/Fixture/PowersFixture.php
+++ tests/Fixture/PowersFixture.php
@@ -8,26 +8,6 @@ use Cake\TestSuite\Fixture\TestFixture;
 class PowersFixture
 	extends TestFixture
 {
-<<<<<<< HEAD
-
-	public $fields =
-		[ 'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null]
-		, 'name' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null]
-		, 'player_text' => ['type' => 'text', 'length' => null, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null]
-		, 'cs_text' => ['type' => 'text', 'length' => null, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null]
-		, 'modified' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null]
-		, 'modifier_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null]
-		, '_constraints' =>
-			[ 'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []]
-			]
-		, '_options' =>
-			[ 'engine' => 'InnoDB'
-			, 'collation' => 'utf8_general_ci'
-			]
-		];
-
-=======
->>>>>>> 0533d1f (tests: use migration for testdb setup instead of fixtures)
 	public function init(): void
 	{
 		$this->records =
