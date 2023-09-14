5ece2c92d72487bc770cfdebf5c33376d58f74bb
Peter van de Werken <peter@van-de-werken.nl>
2023-09-14 20:31:15 +0200

    don't log changes in SocialProfiles to History table

--- src/Model/Table/SocialProfilesTable.php
+++ src/Model/Table/SocialProfilesTable.php
@@ -3,9 +3,22 @@ declare(strict_types=1);
 
 namespace App\Model\Table;
 
-use Cake\Database\Schema\TableSchemaInterface;
+use ArrayObject;
+use Cake\Datasource\EntityInterface;
+use Cake\Event\EventInterface;
 
 class SocialProfilesTable
     extends AppTable
 {
+    public function beforeDelete(EventInterface $event, EntityInterface $entity, ArrayObject $options): void
+    {
+        // don't log changes to History table
+        return;
+    }
+
+    public function beforeSave(EventInterface $event, EntityInterface $entity, ArrayObject $options): void
+    {
+        // don't log changes to History table
+        return;
+    }
 }
