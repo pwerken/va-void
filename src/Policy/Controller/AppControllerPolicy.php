bb01436217befc977202e64a8cf8a3b424ed81ec
Peter van de Werken <peter@van-de-werken.nl>
2024-01-29 20:40:24 +0100

    /admin/<unknown> gives 404 instead of 500

--- src/Policy/Controller/AppControllerPolicy.php
+++ src/Policy/Controller/AppControllerPolicy.php
@@ -5,6 +5,7 @@ namespace App\Policy\Controller;
 
 use Authorization\IdentityInterface as User;
 use Cake\Http\ServerRequest;
+use Cake\Http\Exception\NotFoundException;
 
 use App\Authentication\UnauthenticatedException;
 use App\Policy\AppPolicy;
@@ -17,10 +18,16 @@ class AppControllerPolicy
         $this->setIdentity($identity);
         $action = $request->getParam('action');
 
-        $allowed = $this->{$action}();
-        if(!$allowed and is_null($identity)) {
+        if(method_exists($this, $action)) {
+            $allowed = $this->{$action}();
+            if(!$allowed and is_null($identity)) {
+                throw new UnauthenticatedException();
+            }
+            return $allowed;
+        }
+        if(is_null($identity)) {
             throw new UnauthenticatedException();
         }
-        return $allowed;
+        throw new NotFoundException();
     }
 }
