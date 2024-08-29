ca7ab07888167e2044b24f740a91170ada7f370c
Peter van de Werken <peter@van-de-werken.nl>
2024-08-29 18:56:48 +0200

    Revert "don't update entity if nothing changed"
    This reverts commit 55b660efd22f403bd1d557a9a7fb4f9f057919d1.
    Want ik deed net een wijziging die ik nu niet terug zie in de history.

--- src/Model/Table/AppTable.php
+++ src/Model/Table/AppTable.php
@@ -112,18 +112,10 @@ abstract class AppTable
             return;
         }
 
-        $dirties = [];
-        foreach($entity->getDirty() as $dirty) {
-            if($dirty == 'modified' || $dirty == 'modifier_id') {
-                continue;
-            }
-            if($this->hasField($dirty)) {
-                $dirties[] = $dirty;
-            }
-        }
-        if(empty($dirties)) {
-            $entity->modified = $entity->getOriginal('modified');
-            $entity->modifier_id = $entity->getOriginal('modifier_id');
+        if($entity->isDirty('modified') && $entity->isDirty('modifier_id')
+        && $entity->get('modifier_id') == $entity->getOriginal('modifier_id')
+        && count($entity->getDirty()) == 2)
+        {
             return;
         }
 
