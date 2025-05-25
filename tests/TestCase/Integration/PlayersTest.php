ced2242c77336dd2af6ac711ed2b382386e91232
Peter van de Werken <peter@van-de-werken.nl>
2025-05-25 13:44:50 +0200

    Tests: expand Player testcases

--- tests/TestCase/Integration/PlayersTest.php
+++ tests/TestCase/Integration/PlayersTest.php
@@ -7,11 +7,6 @@ use App\Test\TestSuite\AuthIntegrationTestCase;
 
 class PlayersTest extends AuthIntegrationTestCase
 {
-    public array $fixtures = [
-        'app.Characters',
-        'app.Players',
-    ];
-
     public function checkList($url, $size)
     {
         $data = $this->assertGet($url);
@@ -60,7 +55,7 @@ class PlayersTest extends AuthIntegrationTestCase
         $this->assertArrayHasKey('socials', $data);
     }
 
-    public function testUnauthenticatedAccess()
+    public function testAuthorization()
     {
         $this->withoutAuth();
         $this->assertGet('/players', 401);
@@ -70,17 +65,16 @@ class PlayersTest extends AuthIntegrationTestCase
         $this->assertGet('/players/2/characters', 401);
         $this->assertGet('/players/99', 401);
         $this->assertGet('/players/99/characters', 401);
-    }
+        $this->assertPut('/players', [], 401);
+        $this->assertPut('/players/1', [], 401);
+        $this->assertPut('/players/1/characters', [], 401);
+        $this->assertPut('/players/2', [], 401);
+        $this->assertPut('/players/2/characters', [], 401);
+        $this->assertPut('/players/99', [], 401);
+        $this->assertPut('/players/99/characters', [], 401);
 
-    public function testGetAsPlayer()
-    {
         $this->withAuthPlayer();
-
-        $list = $this->checkList('/players', 1);
-        $this->checkPlayerCompact(1, $list[0]);
-
-        $this->checkPlayer('/players/1', 1);
-
+        $this->assertGet('/players');
         $this->assertGet('/players/1/characters');
         $this->assertGet('/players/1/socials');
         $this->assertGet('/players/2', 403);
@@ -89,9 +83,140 @@ class PlayersTest extends AuthIntegrationTestCase
         $this->assertGet('/players/99', 404);
         $this->assertGet('/players/99/characters', 404);
         $this->assertGet('/players/99/socials', 404);
+        $this->assertPut('/players', [], 403);
+        $this->assertPut('/players/1', []);
+        $this->assertPut('/players/1/characters', [], 403);
+        $this->assertPut('/players/2', [], 403);
+        $this->assertPut('/players/2/characters', [], 403);
+        $this->assertPut('/players/99', [], 404);
+        $this->assertPut('/players/99/characters', [], 403);
+
+        $this->withAuthReadOnly();
+        $this->assertGet('/players');
+        $this->assertGet('/players/1');
+        $this->assertGet('/players/1/characters');
+        $this->assertGet('/players/2');
+        $this->assertGet('/players/2/characters');
+        $this->assertGet('/players/99', 404);
+        $this->assertGet('/players/99/characters', 404);
+        $this->assertPut('/players', [], 403);
+        $this->assertPut('/players/1', [], 403);
+        $this->assertPut('/players/1/characters', [], 403);
+        $this->assertPut('/players/2', []);
+        $this->assertPut('/players/2/characters', [], 403);
+        $this->assertPut('/players/99', [], 404);
+        $this->assertPut('/players/99/characters', [], 403);
+
+        $this->withAuthReferee();
+        $this->assertGet('/players');
+        $this->assertGet('/players/1');
+        $this->assertGet('/players/1/characters');
+        $this->assertGet('/players/2');
+        $this->assertGet('/players/2/characters');
+        $this->assertGet('/players/99', 404);
+        $this->assertGet('/players/99/characters', 404);
+        $this->assertPut('/players', [], 403);
+        $this->assertPut('/players/1', [], 403);
+        $this->assertPut('/players/1/characters', [], 422);
+        $this->assertPut('/players/2', [], 403);
+        $this->assertPut('/players/2/characters', [], 422);
+        $this->assertPut('/players/99', [], 404);
+        $this->assertPut('/players/99/characters', [], 422); // FIXME -> 404
     }
 
-    public function testGetAsReadonly($asReadOnly = true)
+    public function testAddPlayerMinimal()
+    {
+        $input = [
+# only required fields:
+            'plin' => 10,
+            'first_name' => 'first',
+            'last_name' => 'last',
+        ];
+
+        $expected = [
+            'class' => 'Player',
+            'url' => '/players/10',
+            'plin' => 10,
+            'first_name' => $input['first_name'],
+            'insertion' => null,
+            'last_name' => $input['last_name'],
+            'full_name' => $input['first_name'] . ' ' . $input['last_name'],
+            'email' => null,
+            'modifier_id' => self::$PLIN_INFOBALIE,
+        ];
+
+        $this->withAuthInfobalie();
+        $actual = $this->assertPut('/players', $input, 201);
+
+        foreach ($expected as $key => $value) {
+            $this->assertArrayKeyValue($key, $value, $actual);
+        }
+        $this->assertDateTimeNow($actual['modified']);
+    }
+
+    public function testAddPlayerComplete()
+    {
+        $input = [
+# required fields:
+            'plin' => 10,
+            'first_name' => 'first',
+            'last_name' => 'last',
+# optional fields:
+            'insertion' => 'insertion',
+            'email' => 'email@example.com',
+# ignored fields:
+            'modifier_id' => '99',
+            'ignored' => 'ignored',
+        ];
+
+        $expected = [
+            'class' => 'Player',
+            'url' => '/players/10',
+            'plin' => 10,
+            'first_name' => $input['first_name'],
+            'insertion' => $input['insertion'],
+            'last_name' => $input['last_name'],
+            'full_name' => $input['first_name'] . ' ' . $input['insertion'] . ' ' . $input['last_name'],
+            'email' => $input['email'],
+            'modifier_id' => self::$PLIN_INFOBALIE,
+        ];
+
+        $this->withAuthInfobalie();
+        $actual = $this->assertPut('/players', $input, 201);
+
+        foreach ($expected as $key => $value) {
+            $this->assertArrayKeyValue($key, $value, $actual);
+        }
+        $this->assertDateTimeNow($actual['modified']);
+
+        $this->assertArrayNotHasKey('ignored', $actual);
+    }
+
+    public function testRequiredFieldsValidation()
+    {
+        $this->withAuthInfobalie();
+        $response = $this->assertPut('/players', [], 422);
+
+        $errors = $this->assertErrorsResponse('/players', $response);
+
+        # expected fields with validation errors:
+        $this->assertCount(3, $errors);
+        $this->assertArrayHasKey('id', $errors); // FIXME? -> plin
+        $this->assertArrayHasKey('first_name', $errors);
+        $this->assertArrayHasKey('last_name', $errors);
+    }
+
+    public function testGetIndexAsPlayer()
+    {
+        $this->withAuthPlayer();
+
+        $list = $this->checkList('/players', 1);
+        $this->checkPlayerCompact(1, $list[0]);
+
+        $this->checkPlayer('/players/1', 1);
+    }
+
+    public function testGetIndexAsReadonly($asReadOnly = true)
     {
         if ($asReadOnly) {
             $this->withAuthReadOnly();
@@ -104,30 +229,5 @@ class PlayersTest extends AuthIntegrationTestCase
         $this->checkPlayerCompact(4, $list[3]);
         $this->checkPlayerCompact(5, $list[4]);
         $this->checkPlayerCompact(6, $list[5]);
-
-        $this->assertGet('/players/1');
-        $this->assertGet('/players/1/characters');
-        $this->assertGet('/players/2');
-        $this->assertGet('/players/2/characters');
-        $this->assertGet('/players/99', 404);
-        $this->assertGet('/players/99/characters', 404);
-    }
-
-    public function testGetAsReferee()
-    {
-        $this->withAuthReadOnly();
-        $this->testGetAsReadonly(false);
-    }
-
-    public function testGetAsInfobalie()
-    {
-        $this->withAuthInfobalie();
-        $this->testGetAsReadonly(false);
-    }
-
-    public function testGetAsSuper()
-    {
-        $this->withAuthSuper();
-        $this->testGetAsReadonly(false);
     }
 }
