2a15711a66b006899c43e3f531f67330fba0874b
Peter van de Werken <peter@van-de-werken.nl>
2026-01-31 16:04:17 +0100

    Lammy: fix top right identifier number

--- src/Lammy/PowerLammy.php
+++ src/Lammy/PowerLammy.php
@@ -3,14 +3,14 @@ declare(strict_types=1);
 
 namespace App\Lammy;
 
-class PowerLammy extends ConditionLammy
+class PowerLammy extends CharactersConditionLammy
 {
     public function draw(int $side, ?array $data = null): void
     {
         $data = [];
         $data['type'] = 'Power';
         $data['key'] = 'POIN';
-        $data['id'] = $this->entity->get('id');
+        $data['id'] = $this->entity->get('poin');
         $data['name'] = $this->entity->get('name');
         $data['text'] = $this->entity->get('player_text');
         $data['plin'] = null;
